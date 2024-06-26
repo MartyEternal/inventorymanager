61abf16804d7b7891a83a62f0b5c5de13c03cde6
MartyEternal <imartyl@hotmail.com>
2024-06-26 18:57:56 +1000

    log in functions set, ready for coding

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('inventory/', views.inventory_index, name='index'),
-    path('inventory/<int:item_id>/', views.inventory_detail, name='detail'),
-    path('inventory/create/', views.ItemCreate.as_view(), name='inventory_create'),
-    path('inventory/<int:pk>/update/', views.ItemUpdate.as_view(), name='inventory_update'),
-    path('inventory/<int:pk>/delete/', views.ItemDelete.as_view(), name='inventory_delete'),
+    path('item/', views.item_index, name='item'),
+    path('item/<int:item_id>/', views.item_detail, name='detail'),
+    path('item/create/', views.ItemCreate, name='item_create'),
+    path('item/<int:pk>/update/', views.ItemUpdate, name='item_update'),
+    path('item/<int:pk>/delete/', views.ItemDelete, name='item_delete'),
+    path('logout/', views.logout_user, name='logout'),
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,37 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Item
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# inventory = [
+
+# item = [
 #     {'name': 'Table Fork', 'type': 'utensil', 'description': 'This is a table fork. It is designed for the main dish.', 'quantity_current': 4, 'quantity_max': 50},
 #     {'name': 'Table Spoon', 'type': 'utensil', 'description': 'This is a table spoon. The large spoon is used for serving.', 'quantity_current': 3, 'quantity_max': 50},
 # ]
 
 # Create your views here.
+@login_required
 class ItemCreate(CreateView):
     model = Item
     fields = '__all__'
 
+@login_required
 class ItemUpdate(UpdateView):
   model = Item
   fields = ['type', 'description', 'quantity_current', 'quantity_max']
 
+@login_required
 class ItemDelete(DeleteView):
   model = Item
-  success_url = '/inventory'
+  success_url = '/item'
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def inventory_index(request):
-    inventory = Item.objects.all()
-    return render(request, 'inventory/index.html', {
-        'inventory': inventory
+@login_required
+def item_index(request):
+    item = Item.objects.all()
+    return render(request, 'item/index.html', {
+        'item': item
     })
 
-def inventory_detail(request, item_id):
+@login_required
+def item_detail(request, item_id):
     item = Item.objects.get(id=item_id)
-    return render(request, 'inventory/detail.html', { 'item': item })
+    return render(request, 'item/detail.html', { 'item': item })
+
+def home(request):
+    items = Item.objects.all()
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in, welcome!')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an error logging in, please try again.')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'items': items})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')

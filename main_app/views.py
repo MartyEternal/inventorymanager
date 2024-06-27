8678b7383e17e414c02d73153aedeea2140e87a3
Kiran Ravula <kiranrsp@gmail.com>
2024-06-27 16:38:15 +1000

    Update , delete and CSS changes

--- main_app/views.py
+++ main_app/views.py
@@ -6,44 +6,14 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
-# item = [
-#     {'name': 'Table Fork', 'type': 'utensil', 'description': 'This is a table fork. It is designed for the main dish.', 'quantity_current': 4, 'quantity_max': 50},
-#     {'name': 'Table Spoon', 'type': 'utensil', 'description': 'This is a table spoon. The large spoon is used for serving.', 'quantity_current': 3, 'quantity_max': 50},
-# ]
 
-# Create your views here.
-# @login_required
-# class ItemCreate(CreateView):
-#     model = Item
-#     fields = ['name', 'quantity_current', 'quantity_max', 'description']
-
-# # @login_required
-# class ItemUpdate(UpdateView):
-#   model = Item
-#   fields = ['quantity_current', 'quantity_min', 'quantity_max', 'description']
-
-# # @login_required
-# class ItemDelete(DeleteView):
-#   model = Item
-#   success_url = '/items'
-
-# def home(request):
-#     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-# @login_required
-# def items_index(request):
-#     items = Item.objects.all()
-#     return render(request, 'items/index.html', {
-#         'items': items
-#     })
 
-# # @login_required
-# def items_detail(request, item_id):
-#     item = Item.objects.get(id=item_id)
-#     return render(request, 'items/detail.html', { 'item': item })
+
+
 
 def home(request):
     items = Item.objects.all()
@@ -89,3 +59,12 @@ class CategoryUpdate(UpdateView):
 class CategoryDelete(DeleteView):
     model = Category
     success_url = '/categories'
+
+class ItemUpdate(UpdateView):
+  model = Item
+  fields = ['type', 'description', 'quantity_current','quantity_max']
+
+class ItemDelete(DeleteView):
+  model = Item
+  success_url = '/'
+

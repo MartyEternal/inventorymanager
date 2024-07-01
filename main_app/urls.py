07b915e8f7ad0ff793c81b79f69ace30b2018944
Kiran Ravula <kiranrsp@gmail.com>
2024-07-01 18:26:18 +1000

    Pricing and about

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('pricing/', views.pricing, name='pricing'),
+
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     # categories stuff
--- main_app/views.py
+++ main_app/views.py
@@ -11,6 +11,9 @@ from .forms import SignUpForm
 def about(request):
     return render(request, 'about.html')
 
+def pricing(request):
+    return render(request, 'pricing.html')
+
 def register_user(request):
     if request.method == 'POST':
         form = SignUpForm((request.POST))
